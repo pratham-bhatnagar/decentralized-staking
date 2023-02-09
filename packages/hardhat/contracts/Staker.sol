9649df1e3e037b43f730ffd184af13ca447bc77d
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2023-02-09 22:32:25 +0530

    Fixed Failing test case

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,85 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 
+import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    uint256 public deadline = block.timestamp + 72 hours;
+    uint256 public constant threshold = 1 ether;
+    bool openForWithdraw = false;
+    mapping(address => uint256) public balances;
+    event Stake(address, uint256);
+    ExampleExternalContract public exampleExternalContract;
+
+    modifier notCompleted() {
+        require(!exampleExternalContract.completed(), "Staking Time is over");
+        _;
+    }
+
     constructor(address exampleExternalContractAddress) {
         exampleExternalContract = ExampleExternalContract(
             exampleExternalContractAddress
         );
     }
 
-    ExampleExternalContract public exampleExternalContract;
-
-    mapping(address => uint256) public balances;
-
-    bool public executeCalled;
-    bool public openForWithdraw;
-    uint256 public constant threshold = 1 ether;
-    uint256 public deadline = block.timestamp + 72 hours;
-
-    event Stake(address indexed sender, uint256 amount);
-    event Withdraw(address indexed sender, uint256 amount);
-
-    event Received(address, uint256);
-
     receive() external payable {
-        balances[msg.sender] += msg.value;
-        emit Stake(msg.sender, msg.value);
-        emit Received(msg.sender, msg.value);
+        stake();
     }
 
-    modifier deadlineReached() {
-        uint256 timeRemaining = timeLeft();
-        require(timeRemaining == 0, "Deadline is not reached yet");
-        _;
-    }
-
-    modifier deadlineRemaining() {
-        uint256 timeRemaining = timeLeft();
-        require(timeRemaining > 0, "Deadline is already reached");
-        _;
-    }
-
-    modifier stakeNotCompleted() {
-        bool completed = exampleExternalContract.completed();
-        require(!completed, "staking process already completed");
-        _;
-    }
-
-    function stake() public payable deadlineRemaining stakeNotCompleted {
+    function stake() public payable {
+        require(msg.value > 0, "Staking amount cannot be zero!");
+        require(
+            block.timestamp <= deadline,
+            "You cannot stake after the deadline is passed"
+        );
         balances[msg.sender] += msg.value;
         emit Stake(msg.sender, msg.value);
     }
 
-    function withdraw() public stakeNotCompleted deadlineReached {
-        uint256 userBalance = balances[msg.sender];
-        require(userBalance > 0, "You don't have balance to withdraw");
+    function execute() public notCompleted {
+        require(block.timestamp >= deadline, "Deadline is not over yet!");
+        if (address(this).balance < threshold) {
+            openForWithdraw = true;
+        } else {
+            exampleExternalContract.complete{value: address(this).balance}();
+        }
+    }
 
-        require(address(this).balance < threshold,"Threshold Reached, You cannot withdraw");
-
-        (bool sent, ) = msg.sender.call{value: userBalance}("");
-        require(sent, "Failed to send user the left out balance");
+    function withdraw() public notCompleted {
+        require(openForWithdraw == true, "you can't withdraw yet!");
+        require(
+            address(this).balance < threshold && block.timestamp >= deadline,
+            "you can't withdraw yet!"
+        );
+        uint256 amount = balances[msg.sender];
         balances[msg.sender] = 0;
-
-        emit Withdraw(msg.sender, userBalance);
+        payable(msg.sender).transfer(amount);
     }
 
-    function execute() public stakeNotCompleted deadlineReached {
-        uint256 contractBalance = address(this).balance;
-
-        require(address(this).balance >= threshold, "Threshold not reached");
-        require(executeCalled == false, "Execute is called");
-
-        (bool sent, ) = address(exampleExternalContract).call{
-            value: contractBalance
-        }(abi.encodeWithSignature("complete()"));
-
-        require(sent, "exampleExternalContract.complete failed");
-    }
-
-    function timeLeft() public view returns (uint256 timeleft) {
+    function timeLeft() public view returns (uint256) {
         if (block.timestamp >= deadline) {
             return 0;
         } else {
